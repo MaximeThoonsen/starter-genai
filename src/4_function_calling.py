ea0f7e26863e9cd73d900711a4c9396d9d6b0fac
Maxime Thoonsen <maximet@theodo.fr>
2024-06-06 12:04:01 +0200

    refacto lechat

--- src/4_function_calling.py
+++ src/4_function_calling.py
@@ -6,21 +6,40 @@ from langchain_openai import ChatOpenAI
 
 from tools import RequestsGetTool
 
-request_get_tool = RequestsGetTool(allow_dangerous_requests=True)
-functions = [convert_to_openai_function(request_get_tool)]
 
-llm = ChatOpenAI(model="gpt-4o")
+def get_tool(allow_dangerous_requests=False):
+    return RequestsGetTool(allow_dangerous_requests=allow_dangerous_requests)
 
-string_or_function_info = llm.invoke("Can you tell me a joke using chucknorris.io?", functions=functions)
 
-if 'function_call' in string_or_function_info.additional_kwargs:
-    # get the tool with its name
-    tool_name = string_or_function_info.additional_kwargs["function_call"]["name"]
-    if tool_name == 'requests_post':
-        json_argument = string_or_function_info.additional_kwargs["function_call"]["arguments"]
-        arguments = json.loads(json_argument)
-        tool_response = request_get_tool.run(arguments)
-        response = json.loads(tool_response)
-        pprint(response["value"])
-else:
-    pprint(string_or_function_info.content)
+def get_functions(tool):
+    return [convert_to_openai_function(tool)]
+
+
+def get_model(model_name="gpt-4o"):
+    return ChatOpenAI(model=model_name)
+
+
+def invoke_model(model, query, functions=None):
+    return model.invoke(query, functions=functions)
+
+
+def handle_response(response):
+    if 'function_call' in response.additional_kwargs:
+        tool_name = response.additional_kwargs["function_call"]["name"]
+        if tool_name == 'requests_post':
+            json_argument = response.additional_kwargs["function_call"]["arguments"]
+            arguments = json.loads(json_argument)
+            tool_response = get_tool(allow_dangerous_requests=True).run(arguments)
+            json_response = json.loads(tool_response)
+            pprint(json_response["value"])
+    else:
+        pprint(response.content)
+
+
+if __name__ == "__main__":
+    tool = get_tool(allow_dangerous_requests=True)
+    functions = get_functions(tool)
+    model = get_model()
+    query = "Can you tell me a joke using chucknorris.io?"
+    response = invoke_model(model, query, functions=functions)
+    handle_response(response)
